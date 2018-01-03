b5d28b02122a9674197989a1d07f0332cc4640e4
Chris Pahl <c.pahl@adnymics.com>
2018-01-03 10:35:40 +0100

    tests: fix util import

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,7 +9,7 @@ Test the key related utils.
 import pytest
 
 # Internal:
-from redict.util import _extract_keys, _feed_to_nested
+from redict.util import extract_keys, feed_to_nested
 
 
 SAMPLE_DATA = {
@@ -36,8 +36,8 @@ FLATTENED_DATA = {
 @pytest.mark.unittest
 def test_extract_keys():
     """See if extracting all keys from a dict works"""
-    assert set(_extract_keys({})) == set([])
-    key_values = set(_extract_keys(SAMPLE_DATA))
+    assert set(extract_keys({})) == set([])
+    key_values = set(extract_keys(SAMPLE_DATA))
     assert key_values == FLATTENED_DATA
 
 
@@ -46,10 +46,10 @@ def test_feed_to_nested():
     """See if converting the keys back to dicts seems to work"""
     nested = {}
     for key, value in FLATTENED_DATA:
-        _feed_to_nested(nested, key, value)
+        feed_to_nested(nested, key, value)
 
     assert nested == SAMPLE_DATA
 
-    _feed_to_nested(nested, "a.c.e", 2)
-    _feed_to_nested(nested, "a.c.e.f", {"deep": "nested"})
+    feed_to_nested(nested, "a.c.e", 2)
+    feed_to_nested(nested, "a.c.e.f", {"deep": "nested"})
     assert nested["a"]["c"]["e"]["f"]["deep"] == "nested"
