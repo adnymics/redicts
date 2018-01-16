b63f1952e3f534af15d6a0b8cab4ede974bce766
Chris Pahl <c.pahl@adnymics.com>
2018-01-16 13:28:02 +0100

    Bump version to 1.0.4 for release

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ def read(fname):
 
 setup(
     name='redicts',
-    version='1.0.0',
-    description='save arbitary nested python dicts and objects in redis',
+    version='1.0.4',
+    description='Save arbitary nested python dicts and objects in redis',
     url='http://github.com/adnymics/redicts',
     author='adnymics',
     author_email='dev@adnymics.com',
-    license='???',
+    license='GPLv3',
     package_dir={"": "src"},
     packages=["redicts"],
     setup_requires=['pytest-runner'],
